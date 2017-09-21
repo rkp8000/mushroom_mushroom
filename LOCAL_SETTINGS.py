e186c03d2af31f622171301831cf3d7c1bb27fe1
rkp8000 <rpang.contact@gmail.com>
2017-09-21 15:14:32 -0700

    Delete LOCAL_SETTINGS.py

--- LOCAL_SETTINGS.py
+++
@@ -1,8 +0,0 @@
-# DATA ROOT DIRECTORY
-DATA_ROOT = '/Users/rkp/Dropbox/mushroom_data/data/batch_2'
-
-# POSTGRES CONNECTION PARAMETERS
-
-POSTGRES_USER = 'rkp'
-POSTGRES_PW = 'rkp'
-POSTGRES_DB_NAME = 'mushroom_mushroom'
