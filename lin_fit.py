8ec70a806f31c691075748699207050a23309ebf
rkp8000 <rpang.contact@gmail.com>
2018-01-03 11:06:32 -0800

    Write updated linear fitting functions.

--- lin_fit.py
+++ lin_fit.py
@@ -1,316 +1,227 @@
 import numpy as np
-from sklearn.linear_model import LinearRegression, LogisticRegression
 
-from aux import make_extended_predictor_matrix
+from aux import Generic
 
 
-class ClassifierResult(object):
+def fit_h(x, y, wdw_d, method, params):
     
-    def __init__(self, trial_id, preds, windows, valid, states, states_pred, coefs, clf):
-        self.trial_id = trial_id
-        self.preds = preds
-        self.windows = windows
-        self.valid = valid
-        self.states = states
-        self.states_pred = states_pred
-        self.coefs = coefs
-        self.clf = clf
+    if method == 'built-in':
+        # sklearn built-in method
+
+        model = params['model']
+
+        x_xtd = make_extended_predictor_matrix(
+            vs={'x': x}, windows={'x': wdw_d}, order=['x'])
+
+        valid = np.all(~np.isnan(x_xtd), axis=1) & (~np.isnan(y))
+
+        rgr = model()
+        rgr.fit(x_xtd[valid], y[valid])
+
+        h = rgr.coef_.copy()
+        icpt = rgr.intercept_
         
-        # get number of valid time points
-        self.n_valid = valid.sum()
+    elif method == 'wiener':
         
-        # get fraction of valid time points in walking state
-        self.walk_frac = np.sum(states[valid] == 'W') / self.n_valid
-        
-        # get accuracy (fraction of correctly labeled valid states)
-        if states_pred is not None:
-            self.acc = np.sum(states_pred[valid] == states[valid]) / self.n_valid
-        else:
-            self.acc = np.nan
-            
-            
-def classify_states(trial, preds, windows):
-    """
-    Fit a classifier predicting a trial's paused vs. walking states
-    using activity from a set of neural compartments.
-    
-    :param trial: trial object
-    :param windows: dict of windows to use for each predictive dan
-        (keys are DAN names, vals are tuples of (start, end) time
-            points relative to time point of prediction)
-            
-    :return: ClassifierResult
-    """
-    
-    try:
-        trial.dl
-    except:
-        trial.dl = DataLoader(trial, vel_filt=None)
-    
-    states = trial.dl.state
-    vs = {pred: getattr(trial.dl, pred) for pred in preds}
-    
-    # make extended dan predictor matrix
-    vs_extd = make_extended_predictor_matrix(vs, windows, order=preds)
-    
-    # make valid mask (not nan predictors and not ambiguous state)
-    valid = np.all(~np.isnan(vs_extd), axis=1) & (states != 'A')
-    
-    if len(set(states[valid])) == 2:
-        # at least two states
-        
-        # fit classifier
-        clf = LogisticRegression(n_jobs=-1)
-        clf.fit(vs_extd[valid], states[valid])
-        
-        # make state predictions
-        states_pred = np.repeat('', len(states))
-        states_pred[valid] = clf.predict(vs_extd[valid])
-        
-        # get coefficients
-        window_lens = [windows[pred][1] - windows[pred][0] for pred in preds]
-        splits = np.split(clf.coef_[0], np.cumsum(window_lens)[:-1])
-        
-        coefs = {pred: s for pred, s in zip(preds, splits)}
+        raise NotImplementedError('Weiner fitting not implemented yet.')
         
     else:
-        # fewer than two states
-        clf = None
-        states_pred = None
-        coefs = None
         
-    result = ClassifierResult(
-        trial_id=trial.id, preds=preds, windows=windows, valid=valid,
-        states_pred=states_pred, states=states, coefs=coefs, clf=clf)
-    
-    return result
-
-
-class RegressionResult(object):
-    
-    def __init__(self, trial_id, targ, preds, windows, valid, ys, ys_pred, coefs, rgr, details=None):
-        self.trial_id = trial_id
-        self.targ = targ
-        self.preds = preds
-        self.windows = windows
-        self.valid = valid
-        self.ys = ys
-        self.ys_pred = ys_pred
-        self.coefs = coefs
-        self.rgr = rgr
-
-        if details is None:
-            details = {}
+        raise ValueError('Method not recognized.')
+        
+    return h, icpt
             
-        self.details = details
-        
-        self.n_valid = valid.sum()
 
-        if valid.sum() > 0:
-            # calc R^2 val from truth and prediction
-            self.err = ys_pred - ys
-            self.r2 = (np.nanvar(ys) - np.nanvar(self.err)) / np.nanvar(ys)
-        else:
-            self.err = np.repeat(np.nan, len(ys))
-            self.r2 = np.nan
-
-
-def regress(trial, targ, preds, windows, valid='all'):
+def fit_h_train_test(trial, x_name, y_name, wdw, train_len, test_len, method, params, C):
     """
-    Predict one trial variable using other trial variables through a linear regression
-    model.
+    Fit a filter mapping one trial variable to another.
     
-    :param trial: trial object
-    :param targ: target to predict
-    :param preds: order of predictors to use
-    :param windows: dict of windows to use for each predictor
-        (keys are predictor names, vals are tuples of (start, end) time
-            points relative to time point of prediction)
-    :param valid: mask over points to include in prediction
+    :return: 
+        FitResult object with attributes:
+            trial_name
             
-    :return: RegressionResult
-    """
-    
-    if not all([pred in windows for pred in preds]):
-        raise KeyError('One window must be provided for each predictor.')
-        
-    xs = {pred: getattr(trial.dl, pred) for pred in preds}
-    ys = getattr(trial.dl, targ)
-    
-    # make extended dan predictor matrix
-    xs_extd = make_extended_predictor_matrix(xs, windows, order=preds)
-    
-    if valid == 'all':
-        valid = np.repeat(True, len(ys))
-    elif valid == 'none':
-        valid = np.repeat(False, len(ys))
-        
-    # edit valid to ignore any rows with nans
-    if valid.sum() > 0:
-        valid_ = np.all(~np.isnan(xs_extd), 1) & (~np.isnan(ys))
-        valid = valid & valid_
-        
-    ys = ys.copy()
-    ys[~valid] = np.nan
-    
-    if valid.sum() > 0:
-        # at least one valid time point
-        # fit regression
-        rgr = LinearRegression(n_jobs=-1)
-        rgr.fit(xs_extd[valid], ys[valid])
-        
-        # make target predictions
-        ys_pred = np.repeat(np.nan, len(ys))
-        ys_pred[valid] = rgr.predict(xs_extd[valid])
-        
-        # get coefficients
-        window_lens = [windows[pred][1] - windows[pred][0] for pred in preds]
-        splits = np.split(rgr.coef_, np.cumsum(window_lens)[:-1])
-        
-        coefs = {pred: s for pred, s in zip(preds, splits)}
-        
-    else:
-        # no valid time points
-        rgr = None
-        ys_pred = None
-        coefs = None
-        
-    result = RegressionResult(
-        trial_id=trial.id, targ=targ, preds=preds, windows=windows, valid=valid,
-        ys=ys, ys_pred=ys_pred, coefs=coefs, rgr=rgr)
-    
-    return result
-
-
-class ReconstructionResult(object):
-    
-    def __init__(self, trial_id, windows, clf_rslt, rgr_rslts, ys, ys_pred):
-        self.trial_id = trial_id
-        self.windows = windows
-        self.clf_rslt = clf_rslt
-        self.rgr_rslts = rgr_rslts
-        self.ys = ys
-        self.ys_pred = ys_pred
-        
-        self.errs = {}
-        self.r2s = {}
-        
-        for key in self.ys:
-            if key == 'state':
-                continue
+            x_name
+            y_name
             
-            self.errs[key] = ys_pred[key] - ys[key]
-            self.r2s[key] = (np.nanvar(ys[key]) - np.nanvar(self.errs[key])) / np.nanvar(ys[key])
-        
-
-def reconstruct(trial, windows):
-    """Reconstruct a trial's state, speed, angular velocity, and air tube motion.
-    
-    :param trial: trial object
-    :param windows: dict of windows to use for reconstruction; it should have keys
-        {'state', 'speed', 'v_ang', 'v_air'}, and corresponding values should be
-        dict indicating predictor names and windows (tuple specifying start and end time point)
-    :return ReconstructionResult instance
+            x_wdw
+            x_wdw_dsct
+            
+            train
+            test
+            
+            t
+            x
+            y
+            
+            r2_train
+            y_hat_train
+            
+            r2_test
+            y_hat_test
+            
+            t_h
+            h
     """
-    if not set(windows) == {'state', 'speed', 'v_ang', 'v_air'}:
-        raise KeyError(
-            'Predictor windows must be specified for keys "state", '
-            '"speed", "v_ang", and "v_air".')
+    # get variables of interest
+    t = getattr(trial.dl, 't')
+    x = getattr(trial.dl, x_name)
+    y = getattr(trial.dl, y_name)
     
-    preds = {k: w.keys() for k, w in windows.items()}
+    n_t = len(t)
     
-    # classify states
-    clf_rslt = classify_states(trial, preds['state'], windows['state'])
+    # discretize window and training/test lengths
+    wdw_d = (int(round(wdw[0] / C.DT)), int(round(wdw[1] / C.DT)))
     
-    # create valid mask of walking states
-    valid = trial.dl.state == 'W'
+    train_len_d = int(round(train_len / C.DT))
+    test_len_d = int(round(test_len / C.DT))
+    chunk_len_d = train_len_d + test_len_d
     
-    # fit regression models for speed, v_ang, and v_air
-    rgr_rslts = {
-        'speed': regress(trial, targ='speed', preds=preds['speed'], windows=windows['speed'], valid=valid),
-        'v_ang': regress(trial, targ='v_ang', preds=preds['v_ang'], windows=windows['v_ang'], valid=valid),
+    t_h = np.arange(*wdw_d) * C.DT
+    
+    # extract training chunks from data
+    valid_start = max(-wdw_d[0], 0)
+    valid_end = min(n_t-wdw_d[1]+1, n_t)
+    
+    assert np.all(
+        (~np.isnan(x[valid_start:valid_end]))
+        & (~np.isnan(y[valid_start:valid_end])))
+    
+    n_valid = valid_end - valid_start
+    n_chunks = int((n_valid + test_len_d) / chunk_len_d)
+    
+    train = np.zeros(n_t, bool)
+    test = np.zeros(n_t, bool)
+    
+    x_chunks = np.nan * np.zeros((n_chunks, train_len_d))
+    y_chunks = np.nan * np.zeros((n_chunks, train_len_d))
+    
+    for chunk_ctr in range(n_chunks):
+        
+        chunk_start = valid_start + (chunk_ctr * chunk_len_d)
+        chunk_end = chunk_start + train_len_d
+        
+        x_chunks[chunk_ctr, :] = x[chunk_start:chunk_end]
+        y_chunks[chunk_ctr, :] = y[chunk_start:chunk_end]
+        
+        train[chunk_start:chunk_end] = True
+        test[chunk_end:chunk_end + test_len_d] = True
+    
+    # fit filters for each chunk
+    hs = np.nan * np.zeros((n_chunks, len(t_h)))
+    icpts = np.nan * np.zeros(n_chunks)
+    
+    for chunk_ctr, (x_chunk, y_chunk) in enumerate(zip(x_chunks, y_chunks)):
+        
+        h, icpt = fit_h(x_chunk, y_chunk, wdw_d, method, params)
+        
+        hs[chunk_ctr] = h.copy()
+        icpts[chunk_ctr] = icpt.copy()
+        
+    h_mean = hs.mean(axis=0)
+    icpt_mean = icpts.mean(axis=0)
+    
+    # calculate R2 on test data
+    x_xtd = make_extended_predictor_matrix(vs={x_name: x}, windows={x_name: wdw_d}, order=[x_name])
+    y_hat = x_xtd.dot(h_mean) + icpt_mean
+    
+    y_hat_train = np.nan * np.zeros(n_t)
+    y_hat_train[train] = y_hat[train]
+    
+    y_hat_test = np.nan * np.zeros(n_t)
+    y_hat_test[test] = y_hat[test]
+    
+    r2_train = calc_r2(y, y_hat_train)
+    r2_test = calc_r2(y, y_hat_test)
+    
+    fit_result_dict = {
+        'trial_name': trial.name,
+        'x_name': x_name,
+        'y_name': y_name,
+        'wdw': wdw,
+        'wdw_d': wdw_d,
+        'train': train,
+        'test': test,
+        'train_len': train_len,
+        'test_len': test_len,
+        't': t,
+        'x': x,
+        'y': y,
+        't_h': t_h,
+        'r2_train': r2_train,
+        'r2_test': r2_test,
+        'y_hat_train': y_hat_train,
+        'y_hat_test': y_hat_test,
+        'h': h_mean,
+        'icpt': icpt_mean,
     }
     
-    if trial.expt == 'closed_loop':
-        rgr_rslts['v_air'] = regress(
-            trial, targ='v_air', preds=preds['v_air'], windows=windows['v_air'], valid=valid)
-    elif trial.expt == 'driven_random':
-        rgr_rslts['v_air'] = regress(
-            trial, targ='v_air', preds=preds['v_air'], windows=windows['v_air'], valid='all')
+    return Generic(**fit_result_dict)
+
+
+def make_extended_predictor_matrix(vs, windows, order):
+    """
+    Make a predictor matrix that includes offsets at multiple time points.
+    For example, if vs has 2 keys 'a' and 'b', windows is {'a': (-1, 1),
+    'b': (-1, 2)}, and order = ['a', 'b'], then result rows will look like:
+    
+        [v['a'][t-1], v['a'][t], v['b'][t-1], v['b'][t], v['b'][t+1]]
+        
+    :param vs: dict of 1-D array of predictors
+    :param windows: dict of (start, end) time point tuples, rel. to time point of 
+        prediction, e.g., negative for time points before time point of
+        prediction
+    :param order: order to add predictors to final matrix in
+    :return: extended predictor matrix, which has shape
+        (n, (windows[0][1]-windows[0][0]) + (windows[1][1]-windows[1][0]) + ...)
+    """
+    if not np.all([w[1] - w[0] >= 0 for w in windows.values()]):
+        raise ValueError('Windows must all be non-negative.')
+        
+    n = len(list(vs.values())[0])
+    if not np.all([v.ndim == 1 and len(v) == n for v in vs.values()]):
+        raise ValueError(
+            'All values in "vs" must be 1-D arrays of the same length.')
+        
+    # make extended predictor array
+    vs_extd = []
+    
+    # loop over predictor variables
+    for key in order:
+        
+        start, end = windows[key]
+        
+        # make empty predictor matrix
+        v_ = np.nan * np.zeros((n, end-start))
+
+        # loop over offsets
+        for col_ctr, offset in enumerate(range(start, end)):
+
+            # fill in predictors offset by specified amount
+            if offset < 0:
+                v_[-offset:, col_ctr] = vs[key][:offset]
+            elif offset == 0:
+                v_[:, col_ctr] = vs[key][:]
+            elif offset > 0:
+                v_[:-offset, col_ctr] = vs[key][offset:]
+
+        # add offset predictors to list
+        vs_extd.append(v_)
+
+    # return full predictor matrix
+    return np.concatenate(vs_extd, axis=1)
+
+
+def calc_r2(y, y_hat):
+    """
+    Calculate an R^2 value between a true value
+    and a prediction.
+    """
+    valid = (~np.isnan(y)) & (~np.isnan(y_hat))
+    
+    if valid.sum() == 0:
+        return np.nan
     else:
-        rgr_rslts['v_air'] = None
+        u = np.sum((y[valid] - y_hat[valid]) ** 2)
+        v = np.sum((y[valid] - y_hat[valid].mean()) ** 2)
         
-    # make predictions
-    xs = {}
-    xs_extd = {}
-    valid = {}
-    
-    for key in ['state', 'speed', 'v_ang']:
-        xs[key] = {pr: getattr(trial.dl, pr) for pr in preds[key]}
-        xs_extd[key] = make_extended_predictor_matrix(xs[key], windows[key], order=preds[key])
-    
-    if trial.expt != 'no_air':
-        xs['v_air'] = {pr: getattr(trial.dl, pr) for pr in preds['v_air']}
-        xs_extd['v_air'] = make_extended_predictor_matrix(
-            xs['v_air'], windows['v_air'], order=preds['v_air'])
-        
-    # states
-    valid['state'] = np.all(~np.isnan(xs_extd['state']), axis=1)
-    states_pred = np.repeat('', len(valid['state']))
-    if clf_rslt.clf is not None:
-        states_pred[valid['state']] = clf_rslt.clf.predict(xs_extd['state'][valid['state']])
-    else:
-        states_pred[valid['state']] = 'W'
-    
-    # speed
-    valid['speed'] = np.all(~np.isnan(xs_extd['speed']), axis=1) & (states_pred == 'W')
-    speed_pred = np.repeat(np.nan, len(valid['speed']))
-    # set paused speeds to 0
-    speed_pred[states_pred == 'P'] = 0
-    # predict speed during valid epochs
-    speed_pred[valid['speed']] = rgr_rslts['speed'].rgr.predict(xs_extd['speed'][valid['speed']])
-    
-    # v_ang
-    valid['v_ang'] = np.all(~np.isnan(xs_extd['v_ang']), axis=1) & (states_pred == 'W')
-    v_ang_pred = np.repeat(np.nan, len(valid['v_ang']))
-    # set paused v_ang to 0
-    v_ang_pred[states_pred == 'P'] = 0
-    # predict v_ang during valid epochs
-    v_ang_pred[valid['v_ang']] = rgr_rslts['v_ang'].rgr.predict(xs_extd['v_ang'][valid['v_ang']])
-    
-    # air tube
-    v_air_pred = np.repeat(np.nan, len(trial.dl.state))
-    
-    if trial.expt == 'no_air':
-        # everything nan since no air tube motion
-        valid['v_air'] = np.repeat(False, len(trial.dl.state))
-    elif trial.expt == 'closed_loop':
-        # predictors must exist and fly must be walking
-        valid['v_air'] = np.all(~np.isnan(xs_extd['v_air']), axis=1) & (states_pred == 'W')
-        # set paused v_air to 0
-        v_air_pred[states_pred == 'P'] = 0
-    elif trial.expt == 'driven_random':
-        # predictors must exist but fly can be in any state
-        valid['v_air'] = np.all(~np.isnan(xs_extd['v_air']), axis=1)
-        
-    if trial.expt != 'no_air':
-        # predict v_air during valid epochs
-        v_air_pred[valid['v_air']] = rgr_rslts['v_air'].rgr.predict(
-            xs_extd['v_air'][valid['v_air']])
-        
-    rcn_rslt = ReconstructionResult(
-        trial_id=trial.id, windows=windows, clf_rslt=clf_rslt, rgr_rslts=rgr_rslts,
-        ys={
-            'state': trial.dl.state,
-            'speed': trial.dl.speed,
-            'v_ang': trial.dl.v_ang,
-            'v_air': trial.dl.v_air,
-        },
-        ys_pred={
-            'state': states_pred,
-            'speed': speed_pred,
-            'v_ang': v_ang_pred,
-            'v_air': v_air_pred,
-        })
-    
-    return rcn_rslt
+        return 1 - (u/v)
